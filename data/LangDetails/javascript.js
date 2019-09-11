eaae3d61e8e386eb3a05c095620169a62d781739
SREENATH MP <sreenathmp95@gmail.com>
2019-09-11 22:02:46 +0530

    Added javascript LangDetails in javascript.js

---
+++ data/LangDetails/javascript.js
@@ -0,0 +1,35 @@
+const javascript = {
+               description:
+		'JavaScript, often abbreviated as JS, is a high-level, interpreted scripting language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.',
+	youtube: 'https://www.youtube.com/embed/PkZNo7MFNFg',
+	courses: [],
+	posts: [
+		{
+			title: 'Turbocharged JavaScript Refactoring with Codemods',
+			url:
+				'https://medium.com/airbnb-engineering/turbocharged-javascript-refactoring-with-codemods-b0cae8b326b9'
+		},
+    		{
+			title: 'Using NetInfo API to Improve Algolia’s JavaScript Client',
+			url:
+				'https://blog.algolia.com/netinfo-api-algolia-javascript-client/'
+		},
+    		{
+			title: 'Cross-Domain Browser Window Messaging with HTML5 and Javascript',
+			url:
+				'https://blog.carbonfive.com/2012/08/17/cross-domain-browser-window-messaging-with-html5-and-javascript/'
+		},
+    		{
+			title: 'Composing Synchronous and Asynchronous Functions in JavaScript',
+			url:
+				'https://blog.carbonfive.com/2015/01/29/composing-synchronous-and-asynchronous-functions-in-javascript/'
+		},
+    		{
+			title: 'The Path to JavaScript Next',
+			url:
+				'https://www.ebayinc.com/stories/blogs/tech/the-path-to-javascript-next/'
+		}
+    
+	]
+};
+export default javascript;
