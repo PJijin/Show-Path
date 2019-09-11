edaebf59ac983c6083897ddd8e42d2cd6f872364
SREENATH MP <sreenathmp95@gmail.com>
2019-09-12 00:01:09 +0530

    Added CSS language details

---
+++ data/LangDetails/css.js
@@ -0,0 +1,35 @@
+const css = {
+               description:
+		'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.',
+	youtube: 'https://www.youtube.com/embed/ieTHC78giGQ',
+	courses: [],
+	posts: [
+		{
+			title: 'How to style different sites with one CSS collection',
+			url:
+				'https://medium.com/insider-inc-engineering/how-to-style-different-sites-with-one-css-collection-6f33126d4cdb'
+		},
+    		{
+			title: 'Delightful ways to write reusable CSS using subclasses',
+			url:
+				'https://medium.com/insider-inc-engineering/delightful-ways-to-write-reusable-css-using-subclasses-903e90c9cf87'
+		},
+    		{
+			title: 'Inline CSS at Khan Academy: Aphrodite',
+			url:
+				'http://engineering.khanacademy.org/posts/aphrodite-inline-css.htm'
+		},
+    		{
+			title: 'The Unluckiest Paragraphs: A Tale of CSS and Why Parts of Medium Sometimes Disappear',
+			url:
+				'https://medium.engineering/the-unluckiest-paragraphs-751dd36d2d30'
+		},
+    		{
+			title: 'Taking CSS To Scale: A Pragmatic Approach',
+			url:
+				'https://www.wix.engineering/blog/taking-css-to-scale-a-pragmatic-approach'
+		}
+    
+	]
+};
+export default css;
