7a4312473dca4e23a54c67d437d8ea121cd20e43
Pjijin <Pjijin1@gmail.com>
2019-09-12 23:35:42 +0530

    Added machine learning roadmap

--- data/roadmap/machineLearning.js
+++ data/roadmap/machineLearning.js
@@ -1,6 +1,52 @@
 const machineLearning = [
 	{
-		name: 'Machine Learning'
+		name: 'Machine Learning',
+		children: [
+			{
+				name: 'Python',
+				children: [
+					{ name: 'NumPy' },
+					{ name: 'Scipy' },
+					{ name: 'Scikit-learn' },
+					{ name: 'Theano' },
+					{ name: 'TensorFlow' },
+					{ name: 'Keras' },
+					{ name: 'PyTorch' },
+					{ name: 'Pandas' },
+					{ name: 'Matplotlib' }
+				]
+			},
+			{
+				name: 'R',
+				children: [
+					{ name: 'e1071' },
+					{ name: 'rpart' },
+					{ name: 'igraph' },
+					{ name: 'nnet' },
+					{ name: 'randomForest' },
+					{ name: 'caret' },
+					{ name: 'kernlab' },
+					{ name: 'glmnet' },
+					{ name: 'ROCR' },
+					{ name: 'gbm' }
+				]
+			},
+			{
+				name: 'Javascript',
+				children: [
+					{ name: 'Brain.js' },
+					{ name: 'Synaptic' },
+					{ name: 'Neataptic' },
+					{ name: 'Conventjs' },
+					{ name: 'Webdnn' },
+					{ name: 'Deeplearnjs' },
+					{ name: 'Tensorflow' },
+					{ name: 'Compromise' },
+					{ name: 'Neuro.js' },
+					{ name: 'mljs' }
+				]
+			}
+		]
 	}
 ];
 export default machineLearning;
