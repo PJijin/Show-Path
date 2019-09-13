5628dfb7dabb014c3d7a08a435e92b49d30531d2
Pjijin <Pjijin1@gmail.com>
2019-09-13 18:19:45 +0530

    Added virtual reality roadmap

--- data/roadmap/virtualReality.js
+++ data/roadmap/virtualReality.js
@@ -1,6 +1,17 @@
 const virtualReality = [
 	{
-		name: 'Virtual Reality'
+		name: 'Virtual Reality',
+		children: [
+			{ name: 'CSharp' },
+			{ name: 'C' },
+			{ name: 'Java' },
+			{
+				name: 'Javascript',
+				children: [{ name: 'React 360' }, { name: 'Ar.js' }, { name: 'ArgonJs' }, { name: 'A-Frame' }]
+			},
+			{ name: 'Python' },
+			{ name: 'Swift' }
+		]
 	}
 ];
 export default virtualReality;
