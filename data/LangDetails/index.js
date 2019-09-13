17fec67ecc157794f47e8ea093930287be9c08df
Pjijin <Pjijin1@gmail.com>
2019-09-13 18:45:05 +0530

    Typescript language details added

--- data/LangDetails/index.js
+++ data/LangDetails/index.js
@@ -1,10 +1,12 @@
 import html from './html';
 import javascript from './javascript';
 import css from './css';
+import typescript from './typescript';
 
 const CourseDetails = {
 	html,
 	javascript,
-	css
+	css,
+	typescript
 };
 export default CourseDetails;
---
+++ data/LangDetails/typescript.js
@@ -0,0 +1,22 @@
+const typescript = {
+	description:
+		'TypeScript is an open-source programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript, and adds optional static typing to the language.',
+	youtube: 'https://www.youtube.com/embed/xPEMup5SPTM',
+	courses: [],
+	posts: [
+		{
+			title: 'TypeScript at Google',
+			url: 'http://neugierig.org/software/blog/2018/09/typescript-at-google.html'
+		},
+		{
+			title: 'TypeScript at Lyft',
+			url: 'https://eng.lyft.com/typescript-at-lyft-64f0702346ea'
+		},
+		{
+			title: 'How to Actually Improve Type Safety with the TypeScript Strict Flags',
+			url:
+				'https://www.lucidchart.com/techblog/2018/06/20/how-to-actually-improve-type-safety-with-the-typescript-strict-flags/'
+		}
+	]
+};
+export default typescript;
