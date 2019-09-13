6ff3eb900b138384746c3898f11995c2159da817
Pjijin <Pjijin1@gmail.com>
2019-09-13 18:30:55 +0530

    Added vuejs roadmap

--- data/roadmap/vue.js
+++ data/roadmap/vue.js
@@ -1,6 +1,27 @@
 const vue = [
 	{
-		name: 'Vue'
+		name: 'Vue',
+		children: [
+			{
+				name: 'State Management',
+				children: [
+					{ name: 'Component State' },
+					{
+						name: 'Vuex',
+						children: [
+							{
+								name: 'Data persistence',
+								children: [{ name: 'vuex-persist' }, { name: 'vuex-persistedstate' }]
+							},
+							{ name: 'vuex-shared-mutations' },
+							{ name: 'vuex-loading' }
+						]
+					}
+				]
+			},
+			{ name: 'Routing', children: [{ name: 'vue-router' }] },
+			{ name: 'SSR', children: [{ name: 'Nuxt.Js' }] }
+		]
 	}
 ];
 export default vue;
