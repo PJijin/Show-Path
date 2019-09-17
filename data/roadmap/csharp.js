14d237024467d78b4b1284ad1c5f4c79d67a1711
Daniel Martinez Sarta <daniel.martinez.sarta@gmail.com>
2019-09-17 06:04:18 -0400

    Add CSharp backend details.

---
+++ data/roadmap/csharp.js
@@ -0,0 +1,52 @@
+const csharp = [
+	{
+		name: 'CSharp',
+		children: [
+			{
+				name: 'Testing',
+				children: [
+					{
+						name: 'MSTest',
+						legends: {
+							type: 'good',
+							message: 'Good to Know'
+						}
+					},
+					{ name: 'NUnit' },
+					{ name: 'xUnit.NET' },
+				]
+			},
+			{
+				name: 'Frameworks',
+				children: [
+					{ 
+            name: '.NET Framework (Standard)',
+            legends: {
+							type: 'good',
+							message: 'Good to Know'
+						 }
+          },
+          { name: '.NET Core',
+            legends: {
+							type: 'good',
+							message: 'Good to Know'
+						}
+          }
+				]
+			},
+			{
+				name: 'Package Manager',
+				children: [
+					{
+						name: 'NuGet',
+						legends: {
+							type: 'good',
+							message: 'Good to Know'
+						}
+					}
+				]
+			}
+		]
+	}
+];
+export default python;
