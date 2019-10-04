b2d2668d191c01664f964d7d68d9f1517039a162
Daniel Martinez Sarta <daniel.martinez.sarta@gmail.com>
2019-10-04 11:59:17 -0400

    Add .net data to desktop path

--- data/roadmap/desktop.js
+++ data/roadmap/desktop.js
@@ -5,6 +5,14 @@ const desktop = [
 			{
 				name: 'Javascript',
 				children: [{ name: 'ElectronJs' }, { name: 'Qt' }, { name: 'NW.js' }, { name: 'Vuido' }]
+			},
+			{
+				name: 'Microsoft .NET Framework' ,
+				children: [{name: 'Windows Forms'},{name: 'WPF'},{name: 'WCF'}, {name: 'Console'}]
+			},
+			{
+				name: 'Microsoft .NET Core' ,
+				children: [{name: 'WPF'},{name: 'Worker Services'}, {name: 'Console'}]
 			}
 		]
 	}
