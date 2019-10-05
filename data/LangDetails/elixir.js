426b0fd243eaf688108e8eaad3d06c1991811e95
SREENATH MP <sreenathmp95@gmail.com>
2019-10-05 19:22:52 +0530

    Added more language details

---
+++ data/LangDetails/elixir.js
@@ -0,0 +1,18 @@
+const elixir = {
+	description:
+		'Elixir is a functional, concurrent, general-purpose programming language that runs on the Erlang virtual machine. Elixir builds on top of Erlang and shares the same abstractions for building distributed, fault-tolerant applications. ',
+	youtube: 'https://www.youtube.com/embed/pBNOavRoNL0',
+	url: 'https://elixir-lang.org/',
+	courses: [],
+	posts: [
+		{
+			title: 'How We Learned Elixir—Our Story and Tutorial for Beginners',
+			url: 'https://www.monterail.com/blog/learning-elixir-tutorial-guide'
+		},
+		{
+			title: 'The Best Tutorials To Learn Elixir For Beginners',
+			url: 'https://medium.com/quick-code/the-best-tutorials-to-learn-elixir-for-beginners-3d805ebfe5d3'
+		}
+	]
+};
+export default elixir;
