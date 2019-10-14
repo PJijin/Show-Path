9471edd2c09de73dba4f91f58ef4cd85c5a9ed11
Amir Bizimana <amirbzmn@gmail.com>
2019-10-14 18:40:07 -0400

    added data and roadmap for Kotlin and fix for scala

--- data/LangDetails/kotlin.js
+++ data/LangDetails/kotlin.js
@@ -1,9 +1,38 @@
 const kotlin = {
 	description:
 		'Kotlin is an open source programming language which is gaining popularity with native Android application development. Due to its ability to support both object-oriented and functional programming concepts and similar syntax from other programming languages like C#, Java and Scala, developers find it highly adaptable.',
-	youtube: 'https://www.youtube.com/embed/F9UC9DY-vIU',
+	youtube: 'https://www.youtube.com/embed/H_oGi8uuDpA',
 	url: 'https://www.kotlinlang.org/',
-	courses: [],
-	posts: []
+	courses: [
+		{
+			name: 'Kotlin in 3 hours',
+			url: 'https://www.youtube.com/embed/F9UC9DY-vIU',
+			type: 'Free'
+		},
+		{
+			name: 'Programming Fundamentals with Kotlin',
+			url: 'https://www.youtube.com/embed/AeC4G-H-MQA',
+			type: 'Free'
+		}
+
+	],
+	posts: [
+		{
+			title: 'What is Kotlin ? The Java alternative explained',
+			url: 'https://www.infoworld.com/article/3224868/what-is-kotlin-the-java-alternative-explained.html'
+		},
+		{
+			title: 'Java vs Kotlin - Which language should Android Developers start with',
+			url: 'https://dzone.com/articles/java-vs-kotlin-which-language-android-developer-sh'
+		},
+		{
+			title: 'Kotlin vs Java Programming Language to chose for your Android app',
+			url: 'https://mlsdev.com/blog/kotlin-vs-java'
+		},
+		{
+			title: 'Learn Kotlin for Android in one Day',
+			url: 'https://medium.com/@mayuroks/learn-kotlin-for-android-in-one-day-50aa0bc099b0'
+		}
+	]
 };
 export default kotlin;
---
+++ data/roadmap/kotlin.js
@@ -0,0 +1,44 @@
+const kotlin = [
+    {
+        name: 'Kotlin',
+        children: [
+            {
+                name: 'Testing',
+                children: [
+                    {
+                        name: 'Spek'
+                    },
+                    {
+                        name: 'KotlinTest'
+                    },
+                    {
+                        name: 'JUnit'
+                    }
+                ]
+            },
+            {
+                name: 'Frameworks',
+                children: [
+                    {
+                        name: 'Ktor'
+                    },
+                    {
+                        name: 'Javalin'
+                    },
+                    {
+                        name: 'Anko'
+                    }
+                ]
+            },
+            {
+                name: 'Package Manager',
+                children: [
+                    {
+                        name: 'Gradle'
+                    }
+                ]
+            }
+        ]
+    }
+];
+export default kotlin;
